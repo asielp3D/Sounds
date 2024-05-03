db1e5d6a2ae50f8e27ac1712faa250d64069e492
a <asielmasespacio@gmail.com>
2024-05-03 10:38:50 +0200

    a

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -9,24 +9,62 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField] private AudioMixer _mixer;
     [SerializeField] private string _volumeParameter;
     private Slider _volumeSlider;
+    private Toggle _muteToggle;
+    private bool muted;
 
     void Awake()
     {
         _volumeSlider = GetComponent<Slider>();
+        _muteToggle = GetComponentInChildren<Toggle>();
         _volumeSlider.onValueChanged.AddListener(ChangeVolume);
+        _muteToggle.onValueChanged.AddListener(Mute);
     }
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _volumeSlider.value = PlayerPrefs.GetFloat(_volumeParameter,_volumeSlider.maxValue);
+        string muteValue = PlayerPrefs.GetString(_volumeParameter + "Mute","false");
+        if(muteValue == "False")
+        {
+            muted = false;
+        }
+
+        else if(muteValue == "True")
+        {
+            muted = false;
+        }
+
+        _muteToggle.isOn = !muted;
     }
 
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat(_volumeParameter, _volumeSlider.value);
+        PlayerPrefs.SetString(_volumeParameter + "Mute", muted.ToString());
+    }
 
     void ChangeVolume(float value)
     {
-        _mixer.SetFloat(_volumeParameter, value);
+        _mixer.SetFloat(_volumeParameter, Mathf.Log10(value) * 20);
+    }
+
+    void Mute(bool soundEnabled)
+    {
+        if(soundEnabled)
+        {
+            float lastVolume = PlayerPrefs.GetFloat(_volumeParameter, _volumeSlider.maxValue);
+            _mixer.SetFloat(_volumeParameter, Mathf.Log10 (lastVolume)* 20);
+            muted = false;
+        }
+
+        else
+        {
+           PlayerPrefs.SetFloat(_volumeParameter, _volumeSlider.value);
+           _mixer.SetFloat(_volumeParameter, Mathf.Log10(_volumeSlider.minValue)* 20);
+           muted = true;
+        }
     }
 
 }
